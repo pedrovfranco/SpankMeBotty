ec20984f5a79b6c9935b1cee6de380a086aebef7
pedrovfranco <yurbypedro@hotmail.com>
2020-07-04 18:19:03 +0100

    Replace keywords.txt get with env variable

--- speechRecognizer/constants.py
+++ speechRecognizer/constants.py
@@ -7,26 +7,26 @@ class RecognizeResultType:
 		self.message = message
 		self.filename = filename
 
-recognizer_keywords = None
+recognizer_keywords = os.environ['RECOGNIZER_KEYWORDS']
 
-result = []
-success = True
-with open(path.join(path.dirname(path.realpath(__file__)), '..', 'keywords.txt'), 'r') as infile:
-	lines = infile.read().split('\n');
+# result = []
+# success = True
+# with open(path.join(path.dirname(path.realpath(__file__)), '..', 'keywords.txt'), 'r') as infile:
+# 	lines = infile.read().split('\n');
 
-	for x in lines:
+# 	for x in lines:
 
-		if (x == "" or str.isspace(x)):
-			continue
+# 		if (x == "" or str.isspace(x)):
+# 			continue
 		
-		args = x.split(',')
+# 		args = x.split(',')
 
-		if len(args) == 2:
-			result.append((args[0], float(args[1])))
-		else:
-			print('Error reading file')
-			print('line: ' + x)
-			success = False
+# 		if len(args) == 2:
+# 			result.append((args[0], float(args[1])))
+# 		else:
+# 			print('Error reading file')
+# 			print('line: ' + x)
+# 			success = False
 
-if success:
-	recognizer_keywords = result
+# if success:
+# 	recognizer_keywords = result
