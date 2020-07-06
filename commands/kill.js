8b7bd1b42083290c470c3a1d65716f6c02d41f84
pedrovfranco <yurbypedro@hotmail.com>
2020-07-06 02:34:59 +0100

    Change kill command user list to database

--- commands/kill.js
+++ commands/kill.js
@@ -1,4 +1,5 @@
 const common = require('../common');
+const KillList = require('../database/models/killList');
 
 module.exports = {
 	name: 'kill',
@@ -16,19 +17,37 @@ async function execute(message, args) {
 	}
 
 	message.channel.send("( ͡° ͜ʖ ͡°)=ε/̵͇̿̿/'̿̿ ̿ ̿̿ ̿ ̿   " + args[0]);
-	let guildMember = message.member;
-    
-	if (common.killList.includes(guildMember.user.tag)) {
-		message.channel.send("Say your prayers :)");
+	let guild = message.guild;
 
-		setTimeout((guildMember) => {
-			guildMember.voice.kick();
+	console.log("guild.available = " + guild.available);
 
-		}, 3000, guildMember);
+	let usrID = args[0].substr(3, args[0].length-4);
+	console.log("usrID = " + usrID);
 
-	}
-	else {
-		message.channel.send("Looks like you escaped :(");
-	}
+	guild.members.fetch(usrID)
+	.then((res) => {
+
+		let guildMember = res;
+		let tag = guildMember.user.tag;
+		console.log(tag);
+	
+		KillList.findOne({ tag: tag }).orFail()
+		.then(result => {
+	
+			message.channel.send("Say your prayers :)");
+	
+			setTimeout((guildMember) => {
+				guildMember.voice.kick();
+	
+			}, 3000, guildMember);
+		})
+		.catch(err => {
+			message.channel.send("Looks like you escaped :(");
+		})
+	
+	})
+	.catch((err) => {
+		console.log(err);
+	})
 
 }
--- common.js
+++ common.js
@@ -6,7 +6,6 @@ const path = require('path');
 exports.prefix = ',';
 exports.audioFileFormat = 'flac';
 exports.recognitionServiceEndpoint = process.env.NODE_ENV==='production' ? 'https://spank-me-botty.herokuapp.com' : 'http://localhost:' + (5000);
-exports.killList = ['reddevil21', 'tomasbm'];
 exports.audioFileCounter = 0;
 
 
