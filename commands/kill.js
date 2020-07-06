706c8c91ef4482cf258a0f0e599495a31ac96fb5
pedrovfranco <yurbypedro@hotmail.com>
2020-07-06 03:05:13 +0100

    Fix minor bug on kill command

--- commands/kill.js
+++ commands/kill.js
@@ -16,38 +16,44 @@ async function execute(message, args) {
 		return;
 	}
 
-	message.channel.send("( ͡° ͜ʖ ͡°)=ε/̵͇̿̿/'̿̿ ̿ ̿̿ ̿ ̿   " + args[0]);
-	let guild = message.guild;
-
-	console.log("guild.available = " + guild.available);
-
-	let usrID = args[0].substr(3, args[0].length-4);
-	console.log("usrID = " + usrID);
-
-	guild.members.fetch(usrID)
-	.then((res) => {
-
-		let guildMember = res;
-		let tag = guildMember.user.tag;
-		console.log(tag);
+	try {
+		message.channel.send("( ͡° ͜ʖ ͡°)=ε/̵͇̿̿/'̿̿ ̿ ̿̿ ̿ ̿   " + args[0]);
+		let usrID = args[0].substr(3, args[0].length-4);
 	
-		KillList.findOne({ tag: tag }).orFail()
-		.then(result => {
-	
-			message.channel.send("Say your prayers :)");
-	
-			setTimeout((guildMember) => {
-				guildMember.voice.kick();
-	
-			}, 3000, guildMember);
+		message.guild.members.fetch(usrID)
+		.then((res) => {
+
+			let guildMember = res;
+
+			if (!common.validObject(guildMember.voice.channelID)) {
+				message.channel.send("User is not on a voice channel!");
+				return;
+			}
+
+			let tag = guildMember.user.tag;
+			console.log(tag);
+		
+			KillList.findOne({ tag: tag }).orFail()
+			.then(result => {
+		
+				message.channel.send("Say your prayers :)");
+		
+				setTimeout((guildMember) => {
+					guildMember.voice.kick();
+		
+				}, 3000, guildMember);
+			})
+			.catch(err => {
+				message.channel.send("Looks like you escaped :(");
+			})
+		
 		})
-		.catch(err => {
-			message.channel.send("Looks like you escaped :(");
+		.catch((err) => {
+			message.channel.send("First argument should be @user!");
 		})
+	}
+	catch(err) {
+		message.channel.send("First argument should be @user!");
+	}
 	
-	})
-	.catch((err) => {
-		console.log(err);
-	})
-
 }
