51a652ff644b7ac73c29386624ed622d8ae4af2d
pedrovfranco <yurbypedro@hotmail.com>
2020-08-15 19:13:57 +0100

    Fix regex for youtube link

--- commands/play.js
+++ commands/play.js
@@ -8,7 +8,7 @@ module.exports = {
     args: true,
     minargs: 1,
     alias: ['p'],
-    usage: '<link>',
+    usage: '<youtube_link>',
     execute,
 };
 
@@ -18,14 +18,25 @@ async function execute(message, args) {
     if (args.length === 1) {
 
         const link = args[0];
-        const expression = /(http(s)?\:\/\/(www\.)?)?youtube\.com/g;
+        const expression = /^(https?\:\/\/)?(www\.)?youtube\.com/g;
         
         if (expression.test(link)) {
             let voiceConnection = await message.member.voice.channel.join();
-            voiceConnection.play(ytdl(link));
+
+            try {
+                voiceConnection.play(ytdl(link));
+            }
+            catch (exception) {
+                if (exception.message.startsWith('No video id found')) {
+                    common.alertAndLog(message, "Link must be a video");
+                }
+                else {
+                    common.alertAndLog(message, "Failed to play, the link is probably broken");
+                }
+            }
         }
         else {
-            common.alertAndLog("Must be a youtube link");
+            common.alertAndLog(message, "Must be a youtube link");
         }
 
     }
