e88e6caa3fe395e55f094919b37b64716fca660c
pedrovfranco <yurbypedro@hotmail.com>
2020-08-16 02:54:26 +0100

    Fix bug on deleting same track that plays

--- common/music.js
+++ common/music.js
@@ -10,12 +10,20 @@ exports.voiceConnection;
 exports.currentStream;
 exports.streamDispatcher;
 
+const musicDirectory = path.join(__dirname, '..', 'music_files');
+
 exports.addToQueue = async (message, link) => {
     
     return new Promise( async (resolve, reject) => {
         try {
 
             exports.voiceConnection = await message.member.voice.channel.join();
+
+            await cleanMusicDirectory()
+            .catch(err => {
+                reject(err);
+            });
+
             exports.currentStream = await ytdl(link, { 
                 filter: 'audioonly',
                 quality: 'highestaudio',
@@ -24,16 +32,16 @@ exports.addToQueue = async (message, link) => {
             exports.currentStream.on('info', (info) => {
                 exports.currentVideoDetails = info.videoDetails;
 
-                let filepath = path.join(__dirname, '..', 'music_files', exports.currentVideoDetails.videoId + '.webm')
+                let filepath = path.join(musicDirectory, exports.currentVideoDetails.videoId + '.webm')
                 let videoWritableStream = fs.createWriteStream(filepath);
 
                 let stream = exports.currentStream.pipe(videoWritableStream);
 
                 stream.on('finish', function () {
 
-                    stream = fs.createReadStream(filepath);
+                    exports.currentStream = fs.createReadStream(filepath);
 
-                    exports.streamDispatcher = exports.voiceConnection.play(stream);
+                    exports.streamDispatcher = exports.voiceConnection.play(exports.currentStream);
 
                     exports.streamDispatcher.on('error', err => {
                         console.log(err);
@@ -48,6 +56,42 @@ exports.addToQueue = async (message, link) => {
         }
     })
 
+}
+
+async function cleanMusicDirectory() {
+    return new Promise( async (resolve, reject) => {
+
+        // Prevent read error on already playing stream
+        if (exports.streamDispatcher !== undefined) {
+            await exports.streamDispatcher.pause();
+            await exports.streamDispatcher.destroy();
+        }
+
+        if (exports.currentStream !== undefined) {
+            exports.currentStream.close();
+        }
+
+        fs.readdir(musicDirectory, (err, files) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            for (const file of files) {
+
+                if (file !== '.gitkeep') {
+                    fs.unlink(path.join(musicDirectory, file), err => {
+                        if (err) {
+                            reject(err);
+                            return;
+                        }
+                    });
+                }
+            }
+
+            resolve(true);
+        });
+    });
 }
 
 function playNextSong() {
